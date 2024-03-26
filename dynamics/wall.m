0e878cc4c78e3c4722d0dbe060a0fc086a63e70f
Idriss Chelikh <ichelikh@laas.fr>
2024-03-26 02:08:47 +0100

    working hierarchical QP in some sort (controller solve QP but wierd results)

--- dynamics/wall.m
+++ dynamics/wall.m
@@ -1,11 +1,11 @@
-function f_EE = wall(model, x, x_wall)
+function f_EE = wall(model, x, r_wall, ind_EE)
 % wall is in x-plane with offset x_wall
 
 q = x(1:10);   %[10x1] Generalized coordinates
 dq = x(11:20); % [10X1] Generalized velocities
 
 params = model.parameters.values;
-I_J_EE = eval_jac(model.body(11).kinematics.compute.I_J_IBi, q, [], params);	% [3x10] Arm End-effector position and orientation Jacobian
+I_J_EE = eval_jac(model.body(ind_EE).kinematics.compute.I_J_IBi, q, [], params);	% [3x7] Leg End-effector position and orientation Jacobian
 I_T_EE = model.body(11).kinematics.compute.T_IBi(q, dq, [], params);
 x_EE = I_T_EE(1, 4);
 v_EE = I_J_EE(1,:)*dq;
@@ -13,8 +13,8 @@ v_EE = I_J_EE(1,:)*dq;
 K = 1e3;
 D = 2*sqrt(K);
 
-if x_EE > x_wall
-    f_EE = [K*(x_wall-x_EE) - D*v_EE; 0; 0];
+if x_EE > r_wall
+    f_EE = [K*(r_wall-x_EE) - D*v_EE; 0; 0];
 else
     f_EE = zeros(3,1);
 end
